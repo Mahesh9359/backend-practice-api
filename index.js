7475255ed01ca8f0c6993f5a3e4c2f9629c0bec7
Mahesh Jadhav <Maheshjadhav9359@gmail.com>
2025-12-01 13:26:03 +0530

    Add dotenv configuration and update server port to use environment variable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -188,6 +190,7 @@ app.put("/items/:id", (req, res) => {
   });
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
